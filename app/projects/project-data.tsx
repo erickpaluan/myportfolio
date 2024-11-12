c06d5305ae428c0020aaf722667aa0fb4ada2fb2
Erick Paluan <epaluan@outlook.com>
2024-11-12 11:38:46 -0300

    Project Page Ok

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -13,18 +13,4 @@ export const projects: Project[] = [
       "Projeto criado para a conclusão do quarto semestre do curso de Análise e Desenvolvimento de Sistemas da UNIP Ribeirão Preto - 2023.",
     url: "https://github.com/erickpaluan/pim4",
   },
-  {
-    title: "Project Two",
-    year: 2022,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-  {
-    title: "Project Three",
-    year: 2021,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
 ];
