74551b084fcf257156e72b636b45e9de0a7c0cfe
Erick Paluan <epaluan@outlook.com>
2025-03-08 00:24:34 -0300

    refactor: adicionar título à seção de projetos e melhorar a estrutura do componente de página

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from "next-intl";
 import { setRequestLocale } from "next-intl/server";
+import Link from "next/link";
 import { use } from "react";
 //import PageLayout from "@/components/PageLayout";
 
@@ -16,9 +17,23 @@ export default function PathnamesPage({ params }: Props) {
   const t = useTranslations("Projects");
 
   return (
-    <div className="max-w-[490px]">
-      {t("project-title")}
-      {t("project-description")}
-    </div>
+    <section>
+      <h1 className="mb-8 text-2xl font-medium tracking-tight">{t("title")}</h1>
+      <Link href="https://github.com/erickpaluan/pim4" target="_blank">
+        <div className="flex flex-col">
+          <div className="w-full flex justify-between items-baseline">
+            <span className="text-black dark:text-white font-medium tracking-tight">
+              {t("project-title")}
+            </span>
+            <span className="text-neutral-600 dark:text-neutral-400 tabular-nums text-sm">
+              2023
+            </span>
+          </div>
+          <p className="prose prose-neutral dark:prose-invert pt-3">
+            {t("project-description")}
+          </p>
+        </div>
+      </Link>
+    </section>
   );
 }
