14852242ddf5922a6870d0e7ed49a902da6b2d85
Erick Paluan <epaluan@outlook.com>
2026-08-27 22:14:16 -0300

    chore: update favicon set and add web manifest

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -42,8 +42,15 @@ export const metadata: Metadata = {
     title: metaData.name,
     card: "summary_large_image",
   },
+  manifest: "/site.webmanifest",
   icons: {
-    icon: "/favicon.svg",
+    icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
+    ],
+    apple: "/apple-touch-icon.png",
   },
 };
 
